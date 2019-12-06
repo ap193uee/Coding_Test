6461bb44be3ef4357e371569d1f637322ca9ac71
ap193uee <atinderpalap@gmail.com>
2019-12-06 17:51:01 +0530

    Add sample code format for question 2

---
+++ q2_sample.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+Question 2
+Please refer the README.md for this coding question.
+'''
+
+import os
+import sys
+import re
+
+
+def is_convertible(k, s, t):
+    result = False
+    # Enter your code here which should return True or False
+    
+    return result
+
+
+if __name__ == '__main__':
+    k = 5
+    s = whoareyou
+    t = whoarewe
+    print(is_convertible(k, s, t))
