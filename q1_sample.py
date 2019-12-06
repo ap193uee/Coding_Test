a23214b35407be73655636a35af4e8463a700c1a
ap193uee <atinderpalap@gmail.com>
2019-12-06 17:46:06 +0530

    Add sample format for question 1

---
+++ q1_sample.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+'''
+Question 1
+Please refer the README.md for this coding question.
+'''
+
+import os
+import sys
+import re
+
+
+def extract_date(query_string):
+    date_extracted = ""
+    # Enter your code here which should return the date extracted
+    
+    return date_extracted
+
+
+if __name__ == '__main__':
+    text = "lorem OCT. 04, 2018 lorem ipsum"
+    print(extract_date(text))
